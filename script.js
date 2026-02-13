e7836e47b9e82f6e5a7e6d4f417aa19bc9cedfdc
Abhishek Ranajan <abhishekranjan2005@hotmail.com>
2026-02-14 00:25:49 +0530

    assignment

--- script.js
+++ script.js
@@ -1,63 +1,88 @@
 // // target 3 buttons
 // // target the form event form
 
-// const sampleEvent = [{
-//     title:
-//     date:
-//     category:
-//     description:
+const eventForm = document.getElementById("eventForm");
+const eventTitle = document.getElementById("eventTitle");
+const eventDate = document.getElementById("eventDate");
+const eventCategory = document.getElementById("eventCategory");
+const eventDescription = document.getElementById("eventDescription");
+const clearAllBtn = document.getElementById("clearAllBtn");
+const addSampleBtn = document.getElementById("addSampleBtn");
+const eventContainer = document.getElementById("eventContainer");
+const demoContent = document.getElementById("demoContent");
 
-// }.
+const sampleEvents =
+    [
+        {
+            title: "Web dev",
+            date: "4-5-2026",
+            category: "Workshop",
+            description: "usd ius sijjnsf j snen s"
+        },
+        {
+            title: "Web dev2",
+            date: "4-6-2026",
+            category: "conference",
+            description: "bds iudsfnjn s sijjnsf j snen s"
+        }
+    ]
 
-// {
-//     title:
-//     date:
-//     category:
-//     description:
-// }]
+function createEventCard(eventData) {
+    const card = document.createElement("div");
+    card.className="event-card";
 
-
-
-
-
-
-const eventForm = document.getElementById('eventForm');
-const eventTitle = document.getElementById('eventTitle');
-const eventDate = document.getElementById('eventDate');
-const eventCategory = document.getElementById('eventCategory');
-const eventDescription = document.getElementById('eventDescription');
-const clearAllBtn = document.getElementById('clearAllBtn');
-const addsampleBtn = document.getElementById('addSampleBtn');
-const eventsContainer = document.getElementById('eventsContainer');
-const democontent = document.getElementById('democontent');
-
-
-
-const sampleEvent = 
-[
-{
-
-    title:"Web dev",
-    date:"4-5-2026",
-    category:"workshop",
-    description:"usd ius sijijsahjxdajkhskjb sn"
-
-},
-
-{
-    title:"web dev 2",
-    date: "4-6-2026",
-    category:"conference",
-    description:"this is sanbckabs a dsn "
+    card.innerHTML = `
+   <button class="delete-btn">X</button>
+   <h3>${eventData.title}</h3>
+   <div>${eventData.date}</div>
+   <span>${eventData.category}</span>
+   <p>${eventData.description}</p>
+   `
+    return card;
 }
-]
 
-function CreateEventCard(evenData) {
-    const card = document.getcreateElement("div");
+function addEvent(eventData) {
+    const emptyState = document.querySelector(".empty-state");
 
-    card.inerHtml = `
-    <button class="delete-btn">X</button>
-    <h3>{(eventData.Title)}
+    if (emptyState) emptyState.remove();
+
+    eventContainer.appendChild(createEventCard(eventData));
+}
+eventForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const eventData = {
+        title: eventTitle.value,
+        date: eventDate.value,
+        category: eventCategory.value,
+        description: eventDescription.value
+    }
+    addEvent(eventData);
+
+})
+
+
+clearAllBtn.addEventListener("click", () => {
+    eventContainer.innerHTML = `
+    <div class="empty-state">No events yet. Add your first event!</div>`
+})
+
+addSampleBtn.addEventListener("click", () => {
+    sampleEvents.forEach(addEvent);
+})
+
+
+eventContainer.addEventListener("click", (event) => {
+    //closest (child element) use to target the closest child element of the targeted element
+    const card = event.target.closest(".event-card");//event-card
+    console.log(card,"inside line 73");
+
+
+    if (event.target.classList.contains("delete-btn")) 
+        card.remove();
     
-    `
-}
+
+    if(!eventContainer.querySelector(".event-card"))
+        eventContainer.innerHTML = `
+        <div class="empty-state">No events yet. Add your first event!</div>
+        `
+    });
